d82e49d5c97c33909783053ef1ae633c7f80f8d3
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-06-10 17:47:44 +0700

    menambahkan getline(cin, baris) untuk mendapatkan setiap karakter dalam satu baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -13,6 +13,7 @@ int main() {
     //unlimited loop untuk menulis
     while(true) {
         cout << "- ";
-        
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
     }
 }
